3baca6fbbbe6bc362195c930192aa46625e713d3
stefwy123-star <stefwy123@gmail.com>
2026-03-29 16:31:27 +0200

    new station

---
+++ trail.js
@@ -0,0 +1,12 @@
+function resetTrail() {
+  localStorage.removeItem("station1");
+  localStorage.removeItem("station2");
+  localStorage.removeItem("station3");
+  localStorage.removeItem("hinweis1");
+  localStorage.removeItem("hinweis2");
+  localStorage.removeItem("hinweis3");
+  localStorage.removeItem("punkte");
+  localStorage.removeItem("station1_counted");
+  localStorage.removeItem("station2_counted");
+  localStorage.removeItem("station3_counted");
+}
